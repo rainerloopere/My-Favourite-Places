ae201a43bf8f910a224d953acdec6cc70dd0b3c3
Rainer Loopere <rainer.loopere@gmail.com>
2020-05-16 19:25:38 +0200

    Added new activity to ask marker info

---
+++ app/src/main/java/com/example/myfavouriteplaces/NewMarkerActivity.java
@@ -0,0 +1,42 @@
+package com.example.myfavouriteplaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.List;
+
+public class NewMarkerActivity extends AppCompatActivity {
+  private static final String LOG_TAG = FavouritesActivity.class.getSimpleName();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_new_marker);
+
+    final EditText input_title = findViewById(R.id.input_title);
+    final EditText input_description = findViewById(R.id.input_description);
+
+    Button button_save = findViewById(R.id.button_save);
+    button_save.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        String title = input_title.getText().toString();
+        String description = input_description.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(MapsActivity.EXTRA_TITLE, title);
+        setResult(RESULT_OK,replyIntent);
+        Log.d(LOG_TAG, "End activity");
+        finish();
+      }
+    });
+  }
+
+}
