36b867c6c560a6a03de77f6bdd1eee33b85e732e
Rainer Loopere <rainer.loopere@gmail.com>
2020-05-16 13:12:45 +0200

    Basic config to open map

--- app/src/main/java/com/example/myfavouriteplaces/MapsActivity.java
+++ app/src/main/java/com/example/myfavouriteplaces/MapsActivity.java
@@ -1,16 +1,21 @@
 package com.example.myfavouriteplaces;
 
+import android.widget.Toast;
 import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+    OnInfoWindowClickListener {
 
   private GoogleMap mMap;
 
@@ -18,6 +23,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_maps);
+
     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
     SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
         .findFragmentById(R.id.map);
@@ -27,8 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
   /**
    * Manipulates the map once available. This callback is triggered when the map is ready to be
-   * used. This is where we can add markers or lines, add listeners or move the camera. In this
-   * case, we just add a marker near Sydney, Australia. If Google Play services is not installed on
+   * used. This is where we can add markers or lines, add listeners or move the camera. If Google Play services is not installed on
    * the device, the user will be prompted to install it inside the SupportMapFragment. This method
    * will only be triggered once the user has installed Google Play services and returned to the
    * app.
@@ -37,9 +42,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
   public void onMapReady(GoogleMap googleMap) {
     mMap = googleMap;
 
+//    Onclicklistener to add new favourite locations
+    mMap.setOnMapClickListener(new OnMapClickListener() {
+      @Override
+      public void onMapClick(LatLng latLng) {
+        mMap.addMarker(new MarkerOptions().position(latLng).title("New location"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10.0f));
+      }
+    });
+    mMap.setOnInfoWindowClickListener(this);
+
+
     // Add a marker in Sydney and move the camera
     LatLng sydney = new LatLng(-34, 151);
     mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
     mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+  }
+
+  @Override
+  public void onInfoWindowClick(Marker marker) {
+    Toast.makeText(this,"Info!",Toast.LENGTH_SHORT).show();
   }
 }
